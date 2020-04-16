9929be25658d6f45dacf7c628d5d29354db1e493
unknown <leomasterx10@gmail.com>
2020-04-16 15:20:42 -0300

    Se sube el punto 1 y el git ignore

---
+++ tp2_1_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define N 4
+#define M 5
+using namespace std;
+Int f,c;
+Double mt[N][M];
+
+
+int main(){
+        for(f = 0;f<N; f++)
+                    {
+                    for(f = 0;f<N; f++)
+                    {
+                    cprintf(“%lf ”, mt[f][c]);
+                    }
+                    cprintf(“\n”);
+                    }
+  
+ 
+}
